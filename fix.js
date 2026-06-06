0244d39af3adb3fbbd149bb028ee008cbcfb2226
ingorn <efimenko1122@gmail.com>
2026-06-06 21:30:53 +0300

    fix: детерминированный расчёт

--- fix.js
+++ fix.js
@@ -2,10 +2,11 @@ const fs = require('fs');
 const file = './index.html';
 let code = fs.readFileSync(file, 'utf8');
 
-code = code.replace(
-  'const _alpha = 1 - Math.exp(-16 / 80);',
-  'const _alpha = 1 - Math.exp(-16 / 30);'
-);
+const i = code.indexOf('function getScannerSharedLobbyBeamSample(timestampMs = getNetworkTime()) {');
+const end = code.indexOf('\n}', i) + 2;
 
+const newFn = `function getScannerSharedLobbyBeamSample(timestampMs = getNetworkTime()) {\n  return getScannerSharedLobbyBeamSampleAt(timestampMs, "scanner_shared_lobby_motion_v2", scannerRealtime.runtimeData);\n}`;
+
+code = code.slice(0, i) + newFn + code.slice(end);
 fs.writeFileSync(file, code, 'utf8');
-console.log('Done:', code.includes('/ 30)'));
+console.log('Done:', code.includes('getScannerSharedLobbyBeamSampleAt(timestampMs') && !code.includes('_cx'));
